fa3148b68dd11925e6321b7ff56d2ff6f78f002e
Kaliber10 <dev.Kaliber@gmail.com>
2024-04-27 21:25:02 -0400

    Add index page generation
    The index file is now generated. It is generated with a table of
    Pokemon in the database. The table includes links to the respective
    pages. A new module called pydom was created to support generating HTML
    files.

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,7 @@ import yaml
 import build_pokemon
 import traceback
 import pathlib
+import pydom
 
 class DB_Entry():
 
@@ -29,15 +30,42 @@ def main():
     pokemon = _list_db_files(pokemon_db)
     pokemon_pages = pathlib.Path(top).joinpath("pokemon")
     pokemon_pages.mkdir(exist_ok=True)
+    index_table = []
     for p in pokemon:
         try:
             f = open(pathlib.Path(pokemon_pages).joinpath(p.name + ".html"), "w+")
             data = yaml.safe_load(open(p.path, "r"))
             build_pokemon.builder(data, f)
+            row = pydom.TableRow()
+            row.children.append(pydom.TableDataCell())
+            row.children.append(pydom.TableDataCell())
+            row.children[0].text_content = str(data["index"])
+            row.children[1].children.append(pydom.Anchor(href=f"pokemon/{p.name}.html"))
+            row.children[1].children[0].text_content = data["name"]
+            index_table.append(row)
         except:
             traceback.print_exc()
             return 1
 
+    index_table = sorted(index_table, key=lambda x: x.children[0].text_content)
+    dom = pydom.DOM()
+    body = dom.top.children[1]
+    body.children.append(pydom.Heading1())
+    body.children.append(pydom.Div())
+    body.children[1].children.append(pydom.Heading2())
+    body.children[1].children.append(pydom.Table())
+    body.children[0].text_content = "Database"
+    body.children[1].children[0].text_content = "Pokemon"
+    t = body.children[1].children[1]
+    t.children.append(pydom.TableRow())
+    t.children[0].children.append(pydom.TableHeader())
+    t.children[0].children.append(pydom.TableHeader())
+    t.children[0].children[0].text_content = "No."
+    t.children[0].children[1].text_content = "Name"
+    for e in index_table:
+        t.children.append(e)
+    index_page = open(pathlib.Path(top).joinpath("index.html"), "w+")
+    dom.print(stream=index_page)
     return 0
 
 if __name__ == "__main__":
