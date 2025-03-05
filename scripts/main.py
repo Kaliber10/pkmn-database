ae9f2eff3af427bf7b2900a47b486437c83a6413
Kaliber10 <dev.Kaliber@gmail.com>
2025-03-05 08:08:25 -0500

    Finish rebase up to same functionality
    The rebase of the HTML generation has reached the point of same
    functionality as the previous version.
    
    The attempt to create a DOM manager in python was removed. Instead there
    are functions and classes to build/format the required HTML pages. Since
    this is just a script to generate HTML pages, it should be easier to
    maintain this version.

--- scripts/main.py
+++ scripts/main.py
@@ -31,6 +31,98 @@ def _list_db_files(db: pathlib.Path):
         name = e.parts[-2] + e.parts[-1].split(".")[0]
         info.append(DBEntry(e.parts[-3], name, str(e)))
     return info
+
+def _html_block_indenter(block: str, indent: int):
+    '''Helper function to indent a block of HTML for cleaniness'''
+    lines = block.split("\n") # split on every new line
+    reformatted = []
+    for line in lines:
+        reformatted.append(" " * indent + line) # This will add
+    return "\n".join(reformatted) # recombine the block together
+
+class PokemonBuilder():
+    '''A instance of Pokemon data that is used to build an HTML page
+
+    This class is supposed to hold the data for a Pokemon and have the functions to output it to HTML.
+    '''
+    def __init__(self, data, indices, previous):
+        # The data is the data for the Pokemon
+        # The indices are the surrounding indices of the pokemon
+        # previous is a tuple of the evolution information for what it evolved from
+        self.name = data["name"]
+        self.category = data["category"]
+        self.index = data["index"]
+        self.types = data["types"]
+        if previous:
+            self.preevolution = {"method" : previous[1], "pokemon": {"name": previous[0]}}
+        self.neighbors = indices
+        if "evolutions" in data:
+            self.evolutions = data["evolutions"]
+        else:
+            self.evolutions = None
+        self.stats = data["stats"]
+
+    def _build_stats_table(self):
+        # This is the order to print the stats
+        stat_order = ["hp", "attack", "defense", "special attack", "special defense", "speed"]
+        table = "<h2>Stats</h2>\n<table>\n"
+        for stat in stat_order:
+            block =   "<tr>\n"
+            block += f"  <th scope=\"row\">{stat}</th>\n"
+            block += f"  <td>{self.stats[stat]}</td>" + "\n"
+            block +=  "  <td style=\"width:255px\">\n"
+            block += f"    <div style=\"background-color:black;height:20px;width:calc(100%*{self.stats[stat]}/255)\"></div>\n"
+            block +=  "  </td>\n"
+            block +=  "</tr>\n"
+            table += _html_block_indenter(block, 2)
+        table += "</table>\n"
+
+        return table
+
+    def build_page(self):
+        page  =  "<!DOCTYPE html>\n"
+        page +=  "<html>\n"
+        page +=  "  <head>\n"
+        page +=  "  </head>\n"
+        page +=  "  <body>\n"
+        page += f"    <h1>{self.name}</h1>\n"
+        types_str = " ".join(self.types)
+        page += f"    <p>{types_str}</p>\n"
+        page += _html_block_indenter(self._build_stats_table(), 4)
+        page +=  "  </body>\n"
+        page +=  "</html>"
+
+        return page
+
+def _build_index_page(index):
+    page  = "<!DOCTYPE html>\n"
+    page += "<html>\n"
+    page += "  <head>\n"
+    page += "  </head>\n"
+    page += "  <body>\n"
+    page += "    <h1>Database</h1>\n"
+    page += "    <div>\n"
+    page += "      <h2>Pokemon</h2>\n"
+    page += "      <table>\n"
+    page += "        <tr>\n"
+    page += "          <th>No.</th>\n"
+    page += "          <th>Name</th>\n"
+    page += "        </tr>\n"
+    for i, data in enumerate(index):
+        block  =  "        <tr>\n"
+        block += f"          <td>{i}</td>\n"
+        block +=  "          <td>\n"
+        block += f"            <a href=\"pokemon/{data[0]}.html\">{data[1]}</a>\n"
+        block +=  "          </td>\n"
+        block +=  "        </tr>\n"
+        page += block
+    page += "      </table>\n"
+    page += "    </div>\n"
+    page += "  </body>\n"
+    page += "</html>"
+
+    return page
+
 # End helper section
 
 def main():
@@ -48,7 +140,8 @@ def main():
     # Get the data to make it easier to build the files
     for entry in pokemon:
         data = yaml.safe_load(open(entry.path, "r"))
-        index_tracker[data['index']] = data["name"]
+        # The index table is used for linking to pages, so the name used for the file is used.
+        index_tracker[data['index']] = (entry.name, data["name"],)
         if "evolutions" in data:
             for evo in data["evolutions"]:
                 evolution_table.append((data["name"], evo["pokemon"]["name"], evo["method"],))
@@ -60,6 +153,23 @@ def main():
     # This will convert the dictionary to a list
     index_tracker = [x[1] for x in sorted(index_tracker.items(), key=lambda x: x[0])]
     # Now all the hard to grab data is collected.
+
+    pokemon_pages = pathlib.Path(top).joinpath("pokemon")
+    for entry in pokemon:
+        try:
+            data = yaml.safe_load(open(entry.path, "r"))
+            builder = PokemonBuilder(data, None, None)
+            with open(pathlib.Path(pokemon_pages).joinpath(entry.name + ".html"), "w+") as f:
+                f.write(builder.build_page())
+        except:
+            traceback.print_exc()
+            return 1
+
+    index_page = _build_index_page(index_tracker)
+    with open(pathlib.Path(top).joinpath("index.html"), "w+") as index:
+        index.write(index_page)
+
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main())
